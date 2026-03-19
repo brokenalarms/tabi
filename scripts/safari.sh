85a0c4005c4a16abcd927818a8b7fc5ad1de5cb1
brokenalarms <brokenalarms@users.noreply.github.com>
2026-03-19 10:30:58 +0800

    restore last safari tab

--- scripts/safari.sh
+++ scripts/safari.sh
@@ -28,12 +28,23 @@ fi
 echo "→ Reloading Safari…"
 osascript -e '
   tell application "Safari"
-    -- Save window bounds before quitting
+    -- Save window bounds and active tab index before quitting
     set savedBounds to {}
+    set savedTabIndices to {}
     if it is running then
       try
         repeat with w in windows
           set end of savedBounds to bounds of w
+          -- Find index of the current tab in this window
+          set currentTab to current tab of w
+          set tabIdx to 0
+          repeat with j from 1 to count of tabs of w
+            if tab j of w = currentTab then
+              set tabIdx to j
+              exit repeat
+            end if
+          end repeat
+          set end of savedTabIndices to tabIdx
         end repeat
       end try
       quit
@@ -64,12 +75,20 @@ osascript -e '
         end repeat
       end if
     end try
-    -- Restore window bounds
+    -- Restore window bounds and active tabs
     try
       set winList to windows
       repeat with i from 1 to count of savedBounds
         if i ≤ (count of winList) then
           set bounds of item i of winList to item i of savedBounds
+        end if
+      end repeat
+      repeat with i from 1 to count of savedTabIndices
+        if i ≤ (count of winList) then
+          set tabIdx to item i of savedTabIndices
+          if tabIdx > 0 and tabIdx ≤ (count of tabs of item i of winList) then
+            set current tab of item i of winList to tab tabIdx of item i of winList
+          end if
         end if
       end repeat
     end try
